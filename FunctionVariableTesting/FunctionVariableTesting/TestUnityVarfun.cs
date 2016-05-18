163071a8ce4ab23e7f33248fc2d5dd1e81e83a1f
Kirt Hullinger <kirt.jh@gmail.com>
2016-05-17 18:56:37 -0600

    Variables and Functions

---
+++ FunctionVariableTesting/FunctionVariableTesting/TestUnityVarfun.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestUnityVarfun : MonoBehaviour
+{
+	int number = 5;
+}
+
---
+++ Summer Project/Assets/Varfun.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+
+public class Varfun : MonoBehaviour{
+
+	int five = 5;
+	void Start(){
+		five = twice (five);
+		Debug.Log (five);
+	}
+	int twice(int number){
+		int ret;
+		ret = number * 2;
+		return ret;
+
+	}
+
+	}
+
