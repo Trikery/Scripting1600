072a0e1ce064b89594d6b2d0a49fb983fe7516ee
Kirt Hullinger <kirt.jh@gmail.com>
2016-06-06 11:26:29 -0600

    New If Statements with Update.  Giant Octo Game Words.

--- Summer Project/Assets/Beginners Learning/NewIfStatement.cs
+++ Summer Project/Assets/Beginners Learning/NewIfStatement.cs
@@ -4,11 +4,16 @@ using System.Collections;
 public class NewIfStatement : MonoBehaviour {
 
 	public bool myBool = false;
-	public int hit = 1;
+	public int hit = 1; 
+	public string myString;
+	const string cheat = "BOOM1";
+	const string cheat1 = "BOOM2";
 
-	void Start () {
-		if (hit > 1 && hit < 10)
-			print (hit);
+	void Update () {
+		if (myString == cheat)
+			print ("KaBOOOOM!!!");
+		else if (myString == cheat1)
+			print ("sploooooosh....");
 		else
 			print (false);
 		}
