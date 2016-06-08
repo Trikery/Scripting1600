98d8459ac6369a6a2515ba2a7be007b8f6e64e5a
Kirt Hullinger <kirt.jh@gmail.com>
2016-06-08 16:05:44 -0600

    New If Statement

--- Summer Project/Assets/Scripts/ExampleScript2.cs
+++ Summer Project/Assets/Scripts/ExampleScript2.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class ExampleScript2 : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public float xValue = 0.0f;
+	public float yValue = 0.0f;
+
+	void SolveXY (){
+		float divideValue = (xValue / yValue);
+
+		if (xValue == 0){
+			print ("You cannot divide a number by Zero");
+			print ("Imagine you have " + yValue + " cookies and you try to divide them among zero friends.  You see, it doesn't make sense.  The cookie monster is sad because he has no cookies, and you are sad because you have no friends.");
+		}
+		else
+			print (xValue + " devided by " + yValue + " equals " + divideValue);
+
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
+	void Start (){
+		SolveXY ();
+
 	
 	}
 }
