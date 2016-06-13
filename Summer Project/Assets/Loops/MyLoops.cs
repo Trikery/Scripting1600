4b335890f99ef52ef30cabdfbab27a7e4ca6daa6
Kirt Hullinger <kirt.jh@gmail.com>
2016-06-13 11:29:51 -0600

    Loops Beginning.  My Brain can't take much more.

---
+++ Summer Project/Assets/Loops/MyLoops.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyLoops : MonoBehaviour {
+
+	public string[] myFamily;
+	string familyArray;
+		
+		// Use this for initialization
+	void Start () {
+		foreach (string _familyName in myFamily) {
+			familyArray += (_familyName + " Loves ");
+
+			print (familyArray);
+		}
+	}
+
+}
