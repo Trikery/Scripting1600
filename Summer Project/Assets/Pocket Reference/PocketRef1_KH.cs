7310e44c2f3cbbfb2d91bf8c52d1edba14904dc3
Kirt Hullinger <kirt.jh@gmail.com>
2016-06-13 10:06:29 -0600

    Updated Pluralsight Files

---
+++ Summer Project/Assets/Pocket Reference/PocketRef1_KH.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PocketRef1_KH : MonoBehaviour {
+
+	static void Main () {
+		int x = 12 * 30;
+		print (x);
+	}
+	void Start (){
+		Main ();
+	}
+
+
+
+}
---
+++ Summer Project/Assets/Pocket Reference/PocketRef2_KH.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PocketRef2_KH : MonoBehaviour {
+
+	void Main (){
+		print (FeetToInches(20));
+		print (FeetToInches(50));
+	
+	}
+	int FeetToInches (int feet){
+		int inches = feet * 12;
+		return inches;
+	}
+}
