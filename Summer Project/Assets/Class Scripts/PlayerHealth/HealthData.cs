ce932886ec2017e93dc6d1729e8e35ed0617c968
Kirt Hullinger <kirt.jh@gmail.com>
2016-06-22 11:22:26 -0600

    HealthDamage Scripts testing instances and references (but it doesn't work right)

---
+++ Summer Project/Assets/Class Scripts/PlayerHealth/HealthData.cs
@@ -0,0 +1,15 @@
+﻿[System.Serializable]
+public class HealthData{
+
+	//this is our players base health data
+	public int health;
+	public int strength;
+	public int speed = 20;
+	private int defence = 100;
+	public string username;
+
+	public int TakeDownDefence () {
+		return defence -= 10;
+	}
+
+}
