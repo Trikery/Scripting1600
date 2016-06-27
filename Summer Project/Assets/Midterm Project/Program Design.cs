540ece6843198b3e7379a2196313f4ecf7c4cc6b
Kirt Hullinger <kirt.jh@gmail.com>
2016-06-27 11:30:49 -0600

    Plans for Midterm Project

---
+++ Summer Project/Assets/Midterm Project/Program Design.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Program_Design : MonoBehaviour {
+
+/*
+ * Write a program (game):
+
+Requirements are:
+
+Variables
+Functions
+If Statements 
+Loops
+Scope and access modifiers
+Text based (Console) Game.
+
+Problem solve the following:
+
+Create power-ups that must
+
+User interaction. (Trigger, Click)
+Add to a variable (health, ammo)
+Create power-up that
+
+Adds an inventory item 
+Create a function that can solve a math problem
+
+Add numbers
+Subtract numbers
+Create a function that runs with a certain answer (If answer is 10 for example, something happens);
+
+Loop and add bonus inventory items
+Uses Instances and references (Object Oriented)
+
+Use short code. Less lines the better!
+
+Enum's or Arrays?
+Character Select: Cotton, Silk, Denim
+PowerUp Select: Frosty Fruit, Firey Fruit, Adreno Fruit.
+	Frosty Fruit:  Extra HP.   Firey Fruit:  Extra Attack Power,  Adreno Fruit:  Attack Twice?
+Enemy Select: cthulian, carnisaur, wolfas
+
+Reference Character Stat, Enemy Stats, and Item Stats
+Click object to begin the fight.
+
+List Character Stats.  Attack and HP
+"Character Ate the ___"
+List Character's New Stats
+"A ____ Appeared"
+List Enemy HP.
+
+"@p Attacks!"
+print new Enemy HP.
+	if enemy HP <= 0,  Enemy is Dead.
+	else Enemy Attacks.  subtract HP.  loop.  (or vice versa)
+Print Hero HP, Print Enemy HP
+
+Enemy is Dead, receive reward.  
+Hero is Dead.  Game Over
+
+Slow test down with  yeild return new WaitForSeconds()
+	*/
+}
