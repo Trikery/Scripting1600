2a5b88b671ef50b1a34b1f49c707351a74c8dcbe
Kirt Hullinger <kirt.jh@gmail.com>
2016-06-29 10:33:27 -0600

    Midterm Project Loop Fix
    It runs now, but doesn't work right.  Moving to other computer.

--- Summer Project/Assets/Midterm Project/BattleSequence.cs
+++ Summer Project/Assets/Midterm Project/BattleSequence.cs
@@ -3,41 +3,35 @@ using System.Collections;
 
 public class BattleSequence : MonoBehaviour {
 	public BattleOperation turns;
+	public ObjectValue lifeStart;
 
-	/*
-		if (Input.GetKeyUp("key")){}    Usually done in Udate or something
-	*/
-	// this just a function that one of my pluralsight videos used.  To make the program execute with a specific key.  Thought it'd be useful sometime.
 
-	void OnMouseDown () 
-	{
+	void OnMouseDown(){
+
 		int enemylife = turns.HeroAttacks ();												//makes functions variables
 		int herolife = turns.EnemyAttacks ();
 
 		print ("The Hero's HP is " + herolife);												//first prints the HP's of the Enemies
 		print ("The Enemy's HP is " + enemylife);
+		turns.HeroAttacks ();
 
+		print ("The Hero deals Damage to Enemy.  The Enemy has " + enemylife + " HP");
 
-		while (enemylife >= 0) 																//loop begins until the enemy is defeated
+		if (enemylife >= 0) { 																//loop begins until the enemy is defeated
+			turns.EnemyAttacks();
+																		//if statement that determines that the hero won.  the break is used to end the loop.  I hope.
+		} else 
 		{
-			print ("The Hero prepares to attack.  Hero has " + herolife + " HP");
-			print ("The Hero Attacked!  Leaving the Enemy with " + enemylife + " HP.");
-			turns.HeroAttacks ();
-			if (enemylife <= 0) 															//if statement that determines that the hero won.  the break is used to end the loop.  I hope.
-			{
-				print (" has defeated the Enemy!  Congratuations!  You Win");
-				break;																		//  !!!???!!!
-			}
-			else 
-			{
-				print ("The Enemy Survived and is retaliating!");
-				turns.EnemyAttacks ();
+			print ("The Hero has defeated the Enemy!  Congratuations!  You Win");
+		}
 
-			}
-		} 
-			print ("The Enemy has defeated you!  You are no more...");
 
-	}
-
+			
+	} 
 
 }
+
+	/*
+		if (Input.GetKeyUp("key")){}    Usually done in Udate or something
+	*/
+
