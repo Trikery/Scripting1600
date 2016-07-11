f3475995ffb1adf9ee08cee35e396275654b575c
Kirt Hullinger <kirt.jh@gmail.com>
2016-07-11 11:33:27 -0600

    Beta

---
+++ Summer Project/Assets/Class Scripts/PowerUps/PowerUpBase.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpBase : MonoBehaviour {
+
+	public int power = 100;
+	public int speed = 10;
+	public int health = 5;
+
+	public PowerUpInventory myPowerUpInventory;
+	void OnMouseDown (){
+		myPowerUpInventory.AddPowerUpToList (this);
+		gameObject.SetActive (false);
+	}
+
+}
