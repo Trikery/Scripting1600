3f178e58bf52d9b93a761a9137593a5a29a0181a
Kirt Hullinger <kirt.jh@gmail.com>
2016-07-13 11:34:55 -0600

    Weapons Firing  (Working with Lists)

---
+++ Summer Project/Assets/Class Scripts/Lists/WeaponBase.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using StudioThorn;
+
+namespace StudioThorn{
+
+	public class WeaponBase : MonoBehaviour {
+
+		public WeaponInventory myWeaponsInventory;
+		//Weapon Name is the GameObject Name
+		public int firePower;
+		public int ammoCount;
+
+		void OnMouseUp(){
+			if (myWeaponsInventory.myWeapons.Count < 2) {
+			myWeaponsInventory.myWeapons.Add (this);  //access Weap
+			gameObject.SetActive (false);  //
+			}
+		}
+	}
+}
---
+++ Summer Project/Assets/Class Scripts/Lists/WeaponInventory.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace StudioThorn{
+
+	public class WeaponInventory : MonoBehaviour {
+
+		public List<StudioThorn.WeaponBase> myWeapons = new List<WeaponBase> ();
+
+		void Update (){
+			if (Input.GetKeyUp (KeyCode.R) && myWeapons.Count > 0) {	//Count, Length, Size are the same, based on the Syntax of whoever made the program
+				myWeapons[0].gameObject.SetActive (true);
+				myWeapons.RemoveAt (0);
+			}
+			if (Input.GetKeyUp (KeyCode.F) && myWeapons.Count > 0 && myWeapons [0].ammoCount > 0) {
+				print ("BANG");
+				myWeapons [0].ammoCount--;
+			} 
+			else if (Input.GetKeyUp (KeyCode.F) && myWeapons.Count > 0) {
+				myWeapons.RemoveAt (0);
+			}
+		}
+	}
+}
