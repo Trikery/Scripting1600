2eca8d9e53bb2278e9c76c5f28a807ca89f73365
Kirt Hullinger <kirt.jh@gmail.com>
2016-07-18 11:32:36 -0600

    MyEnums Class

---
+++ Summer Project/Assets/Class Scripts/Enums/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float healthTimer = 0;
+	public float maxTime = 30;
+
+	void Update () {
+		switch (MyEnums.gameState){
+		case MyEnums.gameStates.Attacked:
+			if (healthTimer < 30) {
+				print (healthTimer);
+				healthTimer = Time.time;
+			} else {
+				MyEnums.gameState = MyEnums.gameStates.Dying;
+				this.gameObject.SetActive (false);
+			}
+			break;
+
+		}
+	}
+}
