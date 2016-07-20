e06cd69c733be440d00b370e29510aa634b49126
Kirt Hullinger <kirt.jh@gmail.com>
2016-07-20 07:26:42 -0600

    Finished Enum/Switch Assignment
    Created A simple Character Select Screen featuring two characters.  Cotton & Silk.

---
+++ Summer Project/Assets/Personal Test/Program Seven/EnumController.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+
+namespace StudioThorn {
+	public class EnumController {
+		public enum GamePlay {StartMenu, CharacterSelection, PickCotton, PickSilk};
+		public static GamePlay playGame = GamePlay.StartMenu;
+	}
+}
---
+++ Summer Project/Assets/Personal Test/Program Seven/GameController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace StudioThorn {
+	public class GameController : MonoBehaviour {
+
+		public GameObject GameControl;
+		// Create a simple Character Select Program'
+		// Select between two characters
+		// Print Name of Character
+		// Make other character vanish
+		// (optional) bring other character object to the center.
+
+		void ChangeGame (){
+			switch (EnumController.playGame) {
+			case EnumController.GamePlay.StartMenu:
+				GameControl.SetActive (false);
+				print ("Press Start!");
+				break;
+			case EnumController.GamePlay.CharacterSelection:
+				GameControl.SetActive (true);
+				print ("Select Your Character!");
+				this.gameObject.SetActive (false);
+				break;
+			}
+		}
+		void Start (){
+			ChangeGame ();
+		}
+
+		void OnMouseDown () {
+			EnumController.playGame = EnumController.GamePlay.CharacterSelection;
+			ChangeGame ();
+		}
+	
+	}
+	
+}
