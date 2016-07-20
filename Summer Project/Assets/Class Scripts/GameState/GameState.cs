79c581759a1287da4bb74018d6ab4e2e784a5bd3
Kirt Hullinger <kirt.jh@gmail.com>
2016-07-20 11:39:40 -0600

    GameStatets Text Test

---
+++ Summer Project/Assets/Class Scripts/GameState/GameState.cs
@@ -0,0 +1,9 @@
+﻿
+public class GameState {
+
+	//this will change the mode of the game to playing in
+	public enum States {Loading, Starting, Playing, Ending};
+	//Use to change states in other scripts
+	public static States currentGameState = States.Loading;
+
+}
---
+++ Summer Project/Assets/Class Scripts/GameState/StartGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+	public GameObject level;
+	void Start (){
+		level.SetActive (false);
+	}
+
+	public void StartThisGame (){
+		level.SetActive (true);
+		GameState.currentGameState = GameState.States.Playing;
+	}
+}
