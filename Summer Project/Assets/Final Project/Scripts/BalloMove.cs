f19c5864c713b7996d1024ea36f99d10f2155f50
Kirt Hullinger <kirt.jh@gmail.com>
2016-08-01 17:48:52 -0600

    Officially Started Final Project.

---
+++ Summer Project/Assets/Final Project/Scripts/BalloMove.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BalloMove : MonoBehaviour {
+
+	private CharacterController character;
+	private Vector3 moveVector;
+	public float moveSpeed = 5;
+
+	void Start () {
+		character = GetComponent<CharacterController> ();
+	}
+
+	void MoveHorizontal (float _myInput) 
+	{
+		moveVector.z = _myInput;
+		moveVector = transform.TransformDirection (moveVector*moveSpeed*Time.deltaTime);
+		character.Move (moveVector);
+	}
+
+	void MoveVertical (float _myInput) 
+	{
+		moveVector.x = _myInput;
+		moveVector = transform.TransformDirection (moveVector*moveSpeed*Time.deltaTime);
+		character.Move (moveVector);
+	}
+
+	void Update () {
+		MoveVertical (Input.GetAxis ("Vertical"));
+		MoveHorizontal (Input.GetAxis ("Horizontal"));
+		 
+	}
+}
---
+++ Summer Project/Assets/Final Project/Scripts/MedalRotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MedalRotation : MonoBehaviour {
+
+	public float rotateSpeed = 5f;
+
+	void Rotate () 
+	{
+		transform.Rotate (rotateSpeed, 0, rotateSpeed);
+	}
+	void Update () {
+		Rotate ();
+	}
+}
