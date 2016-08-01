2cd4c56be4eee94a857b4e03f7f4e298a568593f
Kirt Hullinger <kirt.jh@gmail.com>
2016-08-01 11:38:10 -0600

    Added a Few Changes to Character Movement

--- Summer Project/Assets/Personal Test/ProgramEight/GameEightController.cs
+++ Summer Project/Assets/Personal Test/ProgramEight/GameEightController.cs
@@ -9,5 +9,6 @@ public class GameEightController : MonoBehaviour {
 	void FixedUpdate () {
 		PlayerMove.Move ();
 		StartCoroutine (EneMove.Mob ());
+
 	}
 }
--- Summer Project/Assets/Personal Test/ProgramEight/MovementTest.cs
+++ Summer Project/Assets/Personal Test/ProgramEight/MovementTest.cs
@@ -4,21 +4,22 @@ using System.Collections;
 public class MovementTest : MonoBehaviour {
 
 	public void Move () {
-		if (Input.GetKey (KeyCode.UpArrow) && this.transform.position.z < 8) {
+		if (Input.GetKey (KeyCode.UpArrow)) {
 			transform.Translate (0, 0, 0.5f);
 		}
-		if (Input.GetKey (KeyCode.DownArrow) && this.transform.position.z > -8) {
+		if (Input.GetKey (KeyCode.DownArrow)) {
 			transform.Translate (0, 0, -0.5f);
 		}
-		if (Input.GetKey (KeyCode.RightArrow) && this.transform.position.x < 8) {
+		if (Input.GetKey (KeyCode.RightArrow)) {
 			transform.Translate (.5f, 0, 0);
 		}
-		if (Input.GetKey (KeyCode.LeftArrow) && this.transform.position.x > -8) {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
 			transform.Translate (-.5f, 0, 0);
 		}
-		if (Input.GetKeyDown (KeyCode.Space) && this.transform.position.y < 0f) {
+		if (Input.GetKeyDown (KeyCode.Space)) {
 			transform.Translate ( 0, 400 * Time.deltaTime, 0);
 		}
 	
 	}
+
 }
