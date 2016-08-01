f0b83bb69c310827fe3a2f906e5cc6cb9603ba76
Kirt Hullinger <kirt.jh@gmail.com>
2016-07-31 20:10:42 -0600

    While (at camp){ scripted some stuff};
    Includes some tutorials I did on Unity3D.com and a personal test involving character movement

--- Summer Project/Assets/Personal Test/ProgramEight/MovementTest.cs
+++ Summer Project/Assets/Personal Test/ProgramEight/MovementTest.cs
@@ -3,28 +3,22 @@ using System.Collections;
 
 public class MovementTest : MonoBehaviour {
 
-	public EnemyMovement EneMove;
-
-	IEnumerator Move () {
-		while (Input.GetKey (KeyCode.UpArrow) && this.transform.position.z < 8) {
-			this.transform.Translate (0, 0, 0.5f);
-			yield return new WaitForSeconds (0.5f);
+	public void Move () {
+		if (Input.GetKey (KeyCode.UpArrow) && this.transform.position.z < 8) {
+			transform.Translate (0, 0, 0.5f);
 		}
-		while (Input.GetKey (KeyCode.DownArrow) && this.transform.position.z > -8) {
-			this.transform.Translate (0, 0, -0.5f);
-			yield return new WaitForSeconds (0.5f);
+		if (Input.GetKey (KeyCode.DownArrow) && this.transform.position.z > -8) {
+			transform.Translate (0, 0, -0.5f);
 		}
-		while (Input.GetKey (KeyCode.RightArrow) && this.transform.position.x < 10) {
-			this.transform.Translate (.5f, 0, 0);
-			yield return new WaitForSeconds (0.5f);
+		if (Input.GetKey (KeyCode.RightArrow) && this.transform.position.x < 8) {
+			transform.Translate (.5f, 0, 0);
 		}
-		while (Input.GetKey (KeyCode.LeftArrow) && this.transform.position.x > -1) {
-			this.transform.Translate (-.5f, 0, 0);
-			yield return new WaitForSeconds (0.5f);
+		if (Input.GetKey (KeyCode.LeftArrow) && this.transform.position.x > -8) {
+			transform.Translate (-.5f, 0, 0);
 		}
-	}
-	void Update () {
-		StartCoroutine (Move ());
-		StartCoroutine (EneMove.Mob ());
+		if (Input.GetKeyDown (KeyCode.Space) && this.transform.position.y < 0f) {
+			transform.Translate ( 0, 400 * Time.deltaTime, 0);
+		}
+	
 	}
 }
